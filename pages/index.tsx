682d6eb88e50f7f9c1606d561524bb3d4b9c1991
rezaavoor <reza.avoor@gmail.com>
2021-03-23 13:10:48 +0100

    first page is half-done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,81 @@
 import { css, useTheme } from "@emotion/react";
-import { Dispatch, SetStateAction } from "react";
+import Image from "next/image";
+import Logo from "../components/Logo";
 
-interface Props {
-  darkState: [boolean, Dispatch<SetStateAction<boolean>>];
-}
-
-export default function Home({ darkState: [isDark, setIsDark] }: Props) {
+export default function Home() {
   const theme = useTheme();
   return (
-    <div
-      css={css`
-        height: 300vh;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        padding: 400px;
-      `}
-    >
-      <p>Reza Hosseini</p>
-      <button onClick={() => setIsDark(!isDark)}>Toggle darkmode</button>
+    <div>
+      <div
+        css={css`
+          position: absolute;
+          top: 130px;
+          left: 50px;
+        `}
+      >
+        <div
+          css={css`
+            height: 30vh;
+            position: relative;
+            left: 15px;
+            margin-bottom: 30px;
+            border-left: ${theme.colors.text} 1px solid;
+          `}
+        ></div>
+        <div
+          css={css`
+            display: flex;
+            height: 35vh;
+            flex-direction: column;
+            justify-content: space-between;
+            * {
+              :hover {
+                cursor: pointer;
+              }
+            }
+          `}
+        >
+          <Logo size="30px" name="email" />
+          <Logo size="30px" name="facebook" />
+          <Logo size="30px" name="twitter" />
+          <Logo size="30px" name="linkedin" />
+          <Logo size="30px" name="instagram" />
+          <Logo size="30px" name="github" />
+        </div>
+      </div>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: 80vw;
+          height: 80vh;
+          margin: auto;
+        `}
+      >
+        <div>
+          <p>Hi there! I'm</p>
+          <h1>Reza Hosseini</h1>
+          <h2
+            css={css`
+              margin-top: 70px;
+            `}
+          >
+            I’m a Software Engineer from Sweden.
+          </h2>
+          <p
+            css={css`
+              width: 90%;
+            `}
+          >
+            I have experience in JavaScript, React, NodeJS, PostgreSQL, and a
+            lot more.
+          </p>
+        </div>
+        <div>
+          <Image src="/Me.png" width="364px" height="638px" />
+        </div>
+      </div>
     </div>
   );
 }
