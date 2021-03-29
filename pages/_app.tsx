c2075fa2e32b5a2cdbb711d6f354865384668ee9
rezaavoor <reza.avoor@gmail.com>
2021-03-29 20:23:38 +0200

    add loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,12 @@ import { useState } from "react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import theme from "../styles/theme";
+import NProgres from "nprogress";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgres.start());
+Router.events.on("routeChangeComplete", () => NProgres.done());
+Router.events.on("routeChangeError", () => NProgres.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isDark, setIsDark] = useState(true);
