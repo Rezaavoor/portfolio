535e31826c91290bdc92ea4c1a4e9c7bd8a7da34
rezaavoor <reza.avoor@gmail.com>
2021-03-30 16:31:03 +0200

    remove api route

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from "next"
-import { HelloType } from "../../types"
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const hellow: HelloType = {name: 'Reza', age: 19};
-  res.status(200).json(hellow)
-}
